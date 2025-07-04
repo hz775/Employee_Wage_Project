a4e92c3f981a81cccba56981c7fe050996bf146e
Hemanth <101321145+hz775@users.noreply.github.com>
2025-07-04 19:43:41 +0530

    Calculating_Wage

--- employee.py
+++ employee.py
@@ -4,6 +4,8 @@ class Employee:
     def __init__(self,name):
         self.name=name
         self.is_Present=False
+        self.wage_per_hour = 20
+        self.full_hours = 8
     
     def check_attendance(self):
         attendance=random.randint(0,1)
@@ -13,5 +15,14 @@ class Employee:
         else:
             self.is_Present=False
             print(f"{self.name} is Absent")
+
+    def calculate_full_time_wage(self):
+        if self.is_Present:
+            wage = self.wage_per_hour * self.full_hours
+            print(f"{self.name}'s Full-Time Daily Wage: ₹{wage}")
+        else:
+            print(f"{self.name} earns ₹0 today (Absent)")
+
+    
 
     
--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from employee import Employee
 # usecase1
 emp1=Employee("Hemanth")
 emp1.check_attendance()
+
+# usecase2
+emp1.calculate_full_time_wage()
