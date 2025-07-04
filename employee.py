d4be5abb8ba03f6ac0121240aaa0eabb32b7c17e
Hemanth <101321145+hz775@users.noreply.github.com>
2025-07-04 20:22:29 +0530

    Part_Time_Wage

--- employee.py
+++ employee.py
@@ -1,11 +1,14 @@
 import random
 
 class Employee:
-    def __init__(self,name):
+    def __init__(self,name,worktype):
         self.name=name
         self.is_Present=False
         self.wage_per_hour = 20
         self.full_hours = 8
+        self.part_time_hours = 4
+        self.worktype = worktype.lower()
+
     
     def check_attendance(self):
         attendance=random.randint(0,1)
@@ -17,12 +20,19 @@ class Employee:
             print(f"{self.name} is Absent")
 
     def calculate_full_time_wage(self):
-        if self.is_Present:
-            wage = self.wage_per_hour * self.full_hours
-            print(f"{self.name}'s Full-Time Daily Wage: ₹{wage}")
-        else:
-            print(f"{self.name} earns ₹0 today (Absent)")
+        if self.worktype == "full-time":
+            if self.is_Present:
+                wage = self.wage_per_hour * self.full_hours
+                print(f"{self.name}'s Full-Time Daily Wage: ₹{wage}")
+            else:
+                print(f"{self.name} earns ₹0 today (Absent)")
 
     
 
-    
+    def calculate_part_time_wage(self):
+        if self.worktype == "part-time":
+            if self.is_Present:
+                wage = self.wage_per_hour * self.part_time_hours
+                print(f"{self.name}'s Part-Time Daily Wage: ₹{wage}")
+            else:
+                print(f"{self.name} earns ₹0 today (Absent)")
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from employee import Employee
 
 # usecase1
-emp1=Employee("Hemanth")
+emp1=Employee("Hemanth","full-time")
 emp1.check_attendance()
 
 # usecase2
 emp1.calculate_full_time_wage()
+
+# usecase3
+emp1.calculate_part_time_wage() 
