9941dcbee6b617f820549d95ae8d010c2876537d
Hemanth <101321145+hz775@users.noreply.github.com>
2025-07-05 11:02:50 +0530

    Switch Case

--- employee.py
+++ employee.py
@@ -20,19 +20,28 @@ class Employee:
             print(f"{self.name} is Absent")
 
     def calculate_full_time_wage(self):
-        if self.worktype == "full-time":
-            if self.is_Present:
-                wage = self.wage_per_hour * self.full_hours
-                print(f"{self.name}'s Full-Time Daily Wage: ₹{wage}")
-            else:
-                print(f"{self.name} earns ₹0 today (Absent)")
+        if self.worktype == "full-time" and self.is_Present:
+            return self.wage_per_hour * self.full_hours
+        return 0
 
     
 
     def calculate_part_time_wage(self):
-        if self.worktype == "part-time":
-            if self.is_Present:
-                wage = self.wage_per_hour * self.part_time_hours
-                print(f"{self.name}'s Part-Time Daily Wage: ₹{wage}")
-            else:
-                print(f"{self.name} earns ₹0 today (Absent)")
+        if self.worktype == "part-time" and self.is_Present:
+            return self.wage_per_hour * self.part_time_hours
+        return 0
+
+    def calculate_wage_switch_case(self):
+        if not self.is_Present:
+            print(f"{self.name} earns ₹0 today (Absent)")
+            return
+
+        match self.worktype:
+            case "full-time":
+                wage = self.calculate_full_time_wage()
+                print(f"{self.name}'s Full-Time Wage: ₹{wage}")
+            case "part-time":
+                wage = self.calculate_part_time_wage()
+                print(f"{self.name}'s Part-Time Wage: ₹{wage}")
+            case _:
+                print(f"Invalid work type for {self.name}")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from employee import Employee
 
 # usecase1
-emp1=Employee("Hemanth","full-time")
+emp1=Employee("Hemanth","part-time")
 emp1.check_attendance()
 
 # usecase2
@@ -9,3 +9,11 @@ emp1.calculate_full_time_wage()
 
 # usecase3
 emp1.calculate_part_time_wage() 
+
+# usecase4
+emp1.calculate_wage_switch_case()
+
+
+
+
+
